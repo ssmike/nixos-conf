843224c34739fe56410c1baed97d2facef33c53c
Mikhail Surin <surinmike@gmail.com>
2023-02-15 15:50:55 -0300

    up cnf

--- configuration.nix
+++ configuration.nix
@@ -53,8 +53,10 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
+  services.xserver.desktopManager.plasma5 = {
+    enable = true;
+    excludePackages = with pkgs.libsForQt5; [kwrited];
+  };
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
@@ -87,7 +89,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.michael = {
     isNormalUser = true;
-    description = "Michael";
+    description = "Михаил";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
@@ -99,8 +101,13 @@
       kmail
       tdesktop
       ktorrent
+      pinentry
+      passff-host
+      pass-nodmenu
+
       libsForQt5.kasts
       libsForQt5.kalendar
+      libsForQt5.kleopatra
     ];
   };
 
@@ -122,6 +129,7 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+  programs.dconf.enable = true;
   programs.gnupg.agent = {
      enable = true;
      enableSSHSupport = true;
