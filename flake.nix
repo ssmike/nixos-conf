1a757b4e011c28d573ba813b38c89f4391ef0a11
Mikhail Surin <surinmike@gmail.com>
2023-02-19 10:58:43 -0300

    add clangd

--- flake.nix
+++ flake.nix
@@ -11,13 +11,37 @@
     system = "x86_64-linux";
     lib = nixpkgs.lib;
     pkgs = nixpkgs.legacyPackages.${system};
+    envs = {
+      cpp = let
+              llvm = pkgs.llvmPackages_14;
+            in
+            [
+              pkgs.glibc
+              pkgs.clang-tools
+              llvm.clang
+              llvm.libcxxabi
+            ];
+    };
+    devShell = deps: with pkgs; stdenv.mkDerivation
+          {
+           name = "cpp env";
+           phases = [ "installPhase" ];
+           installPhase = ''
+              mkdir -p $out
+             '';
+           propagatedBuildInputs = deps;
+          };
   in
-  {
+  { # Removes all phases except installPhase
     nixosConfigurations = {
        inherit system;
        ssmike-thinkpad = lib.nixosSystem {
+          specialArgs = {
+            inherit envs;
+          };
           modules = [ ./configuration.nix  ];
        };
     };
+    devShells.cpp = devShell envs.cpp;
   };
 }
