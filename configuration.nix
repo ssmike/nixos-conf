78f6a556503208fd39bba1daeb80021fce09fa78
Mikhail Surin <surinmike@gmail.com>
2023-02-19 11:15:53 -0300

    fix session

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.defaultSession = "plasmawayland";
+  # services.xserver.displayManager.defaultSession = "plasmawayland";
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5 = {
     enable = true;
